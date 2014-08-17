d97fa5611d2bb0d7592cc0b5fce3f15159ea523e
KOUCHANG <grasshopper.like.ant@gmail.com>
2014-08-18 02:02:19 +0900

    pow関数を完成させた(eunit等込み).

--- src/meth.erl
+++ src/meth.erl
@@ -1,7 +1,7 @@
 -module(meth).
 
 -export([
-         decimal_part/1,
+         is_non_neg_integer/1,
 
          float_to_integer_if_possible/1,
 
@@ -9,48 +9,45 @@
          pow_int/2
         ]).
 
-%% @doc 小数部を返す.
--spec decimal_part(float()) -> float().
-decimal_part(Float) ->
-    Float - trunc(Float).
+%% @doc 自然数かどうかを返す.
+-spec is_non_neg_integer(non_neg_integer()) -> true
+    ;                   (neg_integer())     -> false.
+is_non_neg_integer(NonNeg) when NonNeg >= 0 ->
+    true;
+is_non_neg_integer(_) ->
+    false.
 
 %% @doc 浮動小数点数の小数部が0ならば整数を返す.
 -spec float_to_integer_if_possible(float()) -> number().
+float_to_integer_if_possible(Float) when trunc(Float) == Float ->
+    trunc(Float);
 float_to_integer_if_possible(Float) ->
-    case decimal_part(Float) == 0 of
-        true ->
-            trunc(Float);
-        false ->
-            Float
-    end.
+    Float.
 
 %% @doc 冪乗.
 %%
 %% `math:pow/2' と違い, 計算結果が整数値の場合は, `integer()' を返す.
 %% FIXME: `pow(0.5, -1)' のようなときに `integer()' を返す.
-%% FIXME: `pow(5.0, 2.0)' のようなときに `integer()' を返す.
 -spec pow(Base::integer(), Exponent::non_neg_integer()) -> integer()
     ;    (Base::integer(), Exponent::neg_integer())     -> float()
     ;    (Base::float(),   Exponent::number())          -> number().
-pow(Base0, Exponent0) ->
-    Base = float_to_integer_if_possible(Base0),
-    Exponent = float_to_integer_if_possible(Exponent0),
-
-    case is_integer(Base) andalso is_integer(Exponent) andalso Exponent >= 0 of
-        true ->
-            pow_int(Base, Exponent);
-        false ->
-            math:pow(Base, Exponent)
-    end.
+pow(Base, Exponent) when is_float(Base), trunc(Base) == Base ->
+    pow(trunc(Base), Exponent);
+pow(Base, Exponent) when is_float(Exponent), trunc(Exponent) == Exponent ->
+    pow(Base, trunc(Exponent));
+pow(Base, Exponent) when is_integer(Base), is_integer(Exponent), Exponent >= 0 ->
+    pow_int(Base, Exponent);
+pow(Base, Exponent) ->
+    math:pow(Base, Exponent).
 
 %% @doc 結果が整数になるもののみを計算できる冪乗.
 %%
-%% FIXME: `pow(0.5, -1)' のようなときに `integer()' にする.
+%% 条件としては, Baseが `integer()' , Exponentが `non_neg_integer()' のみを扱う.
 -spec pow_int(Base::integer(), Exponent::non_neg_integer()) -> integer().
 pow_int(Base, Exponent) ->
     pow_int(Base, Exponent, 1).
 
 pow_int(_, 0, C) ->
     C;
-pow_int(Base, Exponent, C) ->
-    pow_int(Base * Base, Exponent div 2, case Exponent rem 2 of  0 -> C; 1 -> Base * C end).
+pow_int(Base, Exponent, C) when Exponent > 0 ->
+    pow_int(Base * Base, Exponent div 2, case Exponent rem 2 of 0 -> C; 1 -> Base * C end).
--- test/meth_tests.erl
+++ test/meth_tests.erl
@@ -3,23 +3,58 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+is_non_neg_integer_test_() ->
+    [
+     {"正の整数", ?_assert(meth:is_non_neg_integer(1))},
+     {"0",        ?_assert(meth:is_non_neg_integer(0))},
+     {"負の整数", ?_assertNot(meth:is_non_neg_integer(-1))}
+    ].
+
+float_to_integer_if_possible_test_() ->
+    [
+     {"結果がinteger()になる",
+      [
+       ?_assertEqual(1, meth:float_to_integer_if_possible(1.0)),
+       ?_assertEqual(2, meth:float_to_integer_if_possible(2.0)),
+       ?_assertEqual(3, meth:float_to_integer_if_possible(3.0)),
+       ?_assertEqual(4, meth:float_to_integer_if_possible(4.0))
+      ]},
+
+     {"結果がfloat()のまま",
+      [
+       ?_assertEqual(2.9, meth:float_to_integer_if_possible(2.9)),
+       ?_assertEqual(2.8, meth:float_to_integer_if_possible(2.8)),
+       ?_assertEqual(2.7, meth:float_to_integer_if_possible(2.7)),
+       ?_assertEqual(2.6, meth:float_to_integer_if_possible(2.6))
+      ]}
+    ].
+
 pow_test_() ->
     [
-     {"底が0",                    ?_assertEqual(0,   meth:pow(0,  4))},
-     {"指数が0",                  ?_assertEqual(1,   meth:pow(9,  0))},
-     {"底が自然数で指数が偶数",   ?_assertEqual(25,  meth:pow(5,  2))},
-     {"底が自然数で指数が奇数",   ?_assertEqual(27,  meth:pow(3,  3))},
-     {"底が負の整数で指数が偶数", ?_assertEqual(49,  meth:pow(-7, 2))},
-     {"底が負の整数で指数が奇数", ?_assertEqual(-64, meth:pow(-4, 3))},
-
-     {"指数が適当に大きな数(2#11001)",
-      ?_assertEqual(-847288609443, meth:pow(-3, 25))},
+     {"integer()を返す", ?_assertEqual(9, meth:pow(3, 2))},
 
      {"底, 指数の小数部が0の場合に整数が変える",
       ?_assertEqual(9, meth:pow(3.0, 2.0))},
+     {"底の小数部が0で指数の小数部が0でない",
+      ?_assertEqual(math:pow(3.0, 2.5), meth:pow(3.0, 2.5))},
+     {"指数の小数部が0で底の小数部が0でない",
+      ?_assertEqual(math:pow(2.5, 3.0), meth:pow(2.5, 3.0))},
 
      {"底が整数で指数が負の整数", ?_assertEqual(0.5, meth:pow(2, -1))},
      {"底が浮動小数点数で指数が整数", ?_assertEqual(2.0, meth:pow(0.5, -1))},
      {"底が数で指数が浮動小数点数",
-      ?_assertEqual(0.7071067811865476, meth:pow(0.5, 0.5))}
+      ?_assertEqual(math:pow(0.5, 0.5), meth:pow(0.5, 0.5))}
+    ].
+
+pow_int_test_() ->
+    [
+     {"底が0",                    ?_assertEqual(0,   meth:pow_int(0,  4))},
+     {"指数が0",                  ?_assertEqual(1,   meth:pow_int(9,  0))},
+     {"底が自然数で指数が偶数",   ?_assertEqual(25,  meth:pow_int(5,  2))},
+     {"底が自然数で指数が奇数",   ?_assertEqual(27,  meth:pow_int(3,  3))},
+     {"底が負の整数で指数が偶数", ?_assertEqual(49,  meth:pow_int(-7, 2))},
+     {"底が負の整数で指数が奇数", ?_assertEqual(-64, meth:pow_int(-4, 3))},
+
+     {"指数が適当に大きな数(2#11001)",
+      ?_assertEqual(-847288609443, meth:pow_int(-3, 25))}
     ].
